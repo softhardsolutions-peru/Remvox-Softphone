97a6198f1da8b3006f3a4336a52ec1aa3ae78536
softhardsolutions-peru <softhardsolutionsperu@gmail.com>
2026-08-22 08:34:25 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 window.REMVOX_PUBLIC_CONFIG = {
   downloads: {
     windows: "https://github.com/softhardsolutions-peru/Remvox-Softphone/releases/tag/v5.0", 
-    linux: ""    // URL directa del paquete .DEB
+    linux: "https://github.com/softhardsolutions-peru/Remvox-Softphone/releases/tag/V5.0"
   },
 
   links: {
